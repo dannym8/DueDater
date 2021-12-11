1256e2906a50606082a8abb0ec873500abad156f
Daniel Glinberg <daniel.glinberg@gmail.com>
2021-12-11 14:37:08 -0500

    Added user input storage, and input system
    Signed-off-by: Daniel Glinberg <daniel.glinberg@gmail.com>

--- src/main/java/com/dan/duedater/guiController.java
+++ src/main/java/com/dan/duedater/guiController.java
@@ -4,10 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 
+import java.nio.charset.StandardCharsets;
+
 public class guiController {
+    // text field declaration
+    @FXML
+    public TextField input;
     // labels declaration
     @FXML
     private Label welcomeText;
@@ -23,10 +29,22 @@ public class guiController {
     void exitProgram(ActionEvent event) {
 
     }
+    // Submits user input
+    @FXML
+    private void onEnter(ActionEvent enterKey) {
+        System.out.println(inputStorage());
+        input.clear();
+    }
+    // Stores user input
+    @FXML
+    private String inputStorage() {
+        return this.input.getText();
+    }
+
     // switches toggle button text
     boolean homework = false;
     @FXML
-    void switchToggleText(MouseEvent event) {
+    private void switchToggleText(MouseEvent event) {
         if (!homework) {
             toggle.setText("Test    ");
             homework = true;
