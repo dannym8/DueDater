1713b294640e22010142d468032f28e15f923414
Daniel Glinberg <daniel.glinberg@gmail.com>
2021-12-16 22:09:31 -0500

    added favicon
    Signed-off-by: Daniel Glinberg <daniel.glinberg@gmail.com>

--- src/main/java/com/dan/duedater/Launcher.java
+++ src/main/java/com/dan/duedater/Launcher.java
@@ -3,9 +3,13 @@ package com.dan.duedater;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static jdk.xml.internal.SecuritySupport.getResourceAsStream;
 
 public class Launcher extends Application {
     final static int SCREEN_WIDTH = 715;
@@ -16,6 +20,7 @@ public class Launcher extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("guiView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), SCREEN_WIDTH, SCREEN_HEIGHT);
+        stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("favicon.png"))));
         stage.setMinWidth(715);
         stage.setTitle("DueDater - Keep track of Due Dates");
         stage.setScene(scene);
